7c7e872c37c8051475e183faee03bea89c90d50b
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-10-13 14:50:57 +0300

    ---

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    form_title = models.TextField(null=True)
+    form_body = models.TextField(null=True)
